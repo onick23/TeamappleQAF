61433ca202c42e86c8f94486f1a39f1aebd9333b
alonick <alonick@localhost>
2021-07-25 16:06:32 -0400

    added a new class Addition

---
+++ src/test/java/basic/Addition.java
@@ -0,0 +1,17 @@
+package basic;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		int a;
+		int b;
+		a=25;
+		b=35;
+		
+			int sum;
+			sum = a+b;
+				System.out.println(sum);
+
+	}
+
+}
